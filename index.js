2c58a1ac203e37afd1d51150f3f1f00edc4e98aa
Lawson <lawsonlewis1@live.com>
2023-09-06 14:19:33 +1000

    rename

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/public/static'));
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/static/template/index.html');
+	res.sendFile(__dirname + '/public/static/template/index.html');
 });
 
 io.on('connection', (socket) => {
